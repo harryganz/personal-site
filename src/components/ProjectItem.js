fcf6819b3a6007b2195bfb7cdde3bd9ae13cb9ca
Harry Ganz <hganz@harryganz.com>
2017-09-28 21:43:00 -0400

    Changes link styles for project gallery

--- src/components/ProjectItem.js
+++ src/components/ProjectItem.js
@@ -5,16 +5,18 @@ import 'components/ProjectItem.css';
 function ProjectItem({project}) {
 	return (
 			<div className="project">
-					<a className="crop" href={project.url}>
+				<a className="none" href={project.url}>
+					<div className="crop">
 						<img src={process.env.PUBLIC_URL + project.img_url} alt={project.title} />
-					</a>
+					</div>
 					<h3>{project.title}</h3>
 					<p className="project-description">{project.description}</p>
-					<a className="none github-link" href={"https://github.com/" + project.github}>
-						<i className="fa fa-github-square" aria-hidden="true" />
-						&nbsp;
-						<span>Github Page</span>
-					</a>
+				</a>
+				<a className="github-link" href={"https://github.com/" + project.github}>
+					<i className="fa fa-github-square" aria-hidden="true" />
+					&nbsp;
+					<span>Github Page</span>
+				</a>
 
 				</div>
 			);
