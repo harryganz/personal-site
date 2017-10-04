a82f733c9db1fc4bb2afe5ede3c0d3a19a47e0b4
Harry Ganz <hganz@harryganz.com>
2017-10-03 21:38:43 -0400

    Use flexbox styles for resume containers

--- src/components/Resume.js
+++ src/components/Resume.js
@@ -2,41 +2,33 @@ import React from 'react';
 
 import DefaultLayout from 'components/DefaultLayout';
 import LinearResumeList from 'components/LinearResumeList';
-import LinearResumeItem from 'components/LinearResumeItem';
 
 import 'components/LinearResumeItem.css';
 
 function Resume() {
 	return (
 				<DefaultLayout>
-					<div>
-						<h3>Programming Languages and Frameworks</h3>
-						<LinearResumeList
-							title="Languages"
-							items={ ['Go', 'Python', 'Ruby', 'Java', 'Javascript', 'R'] }
-						/>
-						<LinearResumeList
-							title="Back-End"
-							items={ ['Ruby-on-Rails', 'NodeJS', 'Spring', 'Jekyll'] }
-						/>
-						
-						<div className="linear-resume-list">
-							<p className="title">Front-End</p>
-							<div className="list">
-								<LinearResumeItem>React</LinearResumeItem>
-								<LinearResumeItem>JQuery</LinearResumeItem>
-								<LinearResumeItem>Angular</LinearResumeItem>
-								<LinearResumeItem>Dojo</LinearResumeItem>
-							</div>
+					<h3>Programming Languages and Frameworks</h3>
+					<div className="flex flex-two-col">
+						<div className="flex-grow">
+							<LinearResumeList
+								title="Languages"
+								items={ ['Go', 'Python', 'Ruby', 'Java', 'Javascript', 'R'] }
+							/>
+							<LinearResumeList
+								title="Back-End"
+								items={ ['Ruby-on-Rails', 'NodeJS', 'Spring', 'Jekyll'] }
+							/>
 						</div>
-						<div className="linear-resume-list">
-							<p className="title">Other</p>
-							<div className="list">
-								<LinearResumeItem>Docker</LinearResumeItem>
-								<LinearResumeItem>Webpack</LinearResumeItem>
-								<LinearResumeItem>CSS</LinearResumeItem>
-								<LinearResumeItem>Sass</LinearResumeItem>
-							</div>
+						<div className="flex-grow">
+							<LinearResumeList
+								title="Front-End"
+								items={ ['React', 'JQuery', 'Angular', 'Dojo'] }
+							/>
+							<LinearResumeList
+								title="Other"
+								items={ ['Docker', 'Webpack', 'CSS', 'Sass'] }
+							/>
 						</div>
 					</div>
 				</DefaultLayout>
